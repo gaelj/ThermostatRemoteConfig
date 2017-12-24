6730ca98cc42f79bc8289e49777e9d283aafd83f
gaelj <gaeljames@gmail.com>
2017-12-24 22:01:42 +0100

    Initial commit

---
+++ ThermostatRemoteConfig.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		ThermostatRemoteConfig.cpp
+ Created:	24/12/2017 22:00:55
+ Author:	gael
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "ThermostatRemoteConfig.h"
+
+
---
+++ ThermostatRemoteConfig.h
@@ -0,0 +1,19 @@
+/*
+ Name:		ThermostatRemoteConfig.h
+ Created:	24/12/2017 22:00:55
+ Author:	gael
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _ThermostatRemoteConfig_h
+#define _ThermostatRemoteConfig_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
